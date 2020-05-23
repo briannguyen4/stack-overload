71a5f54d7a294b32a184976f6afe6f100e5059c4
briannguyen4 <brian4nguyen@gmail.com>
2020-05-23 08:11:00 -0400

    edit splash page

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -10,19 +10,17 @@ const Splash = () => (
             <img src={window.splashURL}/>
             <div className="splash-img__text">
                 <h1 className="splash-img__text__header">We &#60;3 people who code</h1>
-                <p className="splash-img__text__sentence">We build products that empower developers</p>
-                <p className="splash-img__text__sentence">and connect them to solutions that enable</p>
-                <p className="splash-img__text__sentence">productivity, growth, and discovery.</p>
+                <p className="splash-img__text__sentence">We build products that empower developers
+                and connect them to solutions that enable productivity, growth, and discovery.</p>
             </div>
         </div>
         <div className="splash-message">
             <h1 className="splash-message__header">For developers, by developers</h1>
             <div className="splash-message__bar"></div>
             <div className="splash-message__text">
-                <p>Welcome to Stack Overload, an open community for all coders.</p> 
-                <p>coders. Get answers immediately from a community of</p>
-                <p>knowledgeable programmers. Search through a collection of</p>
-                <p>questions or create an account to ask your own!</p>
+                <p>Welcome to Stack Overload, an open community for all coders. Get answers immediately
+                 from a community of knowledgeable programmers. Search through a collection of questions
+                 or create an account to ask your own!</p>
             </div>    
         </div>
     </div>
