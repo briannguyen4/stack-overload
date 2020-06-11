5682961169a768da16df3f2c10a56e1000ddc7dc
briannguyen4 <brian4nguyen@gmail.com>
2020-06-11 09:01:00 -0400

    add routes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -14,7 +14,13 @@ const App = () => (
         <AuthRoute exact path='/' component={Splash} />
         <AuthRoute exact path='/login' component={LoginFormContainer} />
         <AuthRoute exact path='/signup' component={SignupFormContainer}/>
-        
+        <ProtectedRoute exact path='/' component={QuestionIndexContainer}/>
+        <Switch>
+            <ProtectedRoute exact path="/questions/:questionId/edit" component={UpdateQuestion} />
+            <ProtectedRoute exact path='/questions/new' component={PostQuestion} />
+            <Route exact path="/questions/:questionId" component={QuestionShowContainer} />
+            <Route exact path='/questions' component={QuestionIndexContainer} />
+        </Switch>        
     </>
 );
 
