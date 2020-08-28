fac907f6cc4d2b98e76c3b9f2aca3fbd0c48110e
briannguyen4 <brian4nguyen@gmail.com>
2020-08-28 10:55:17 -0400

    add list of answer components to question show page

--- frontend/components/questions/question_show.jsx
+++ frontend/components/questions/question_show.jsx
@@ -29,45 +29,45 @@ class QuestionShow extends React.Component {
         return (
           <>
           <AltNavbarContainer id="qsidebar"/>
-            <Sidebar />
-            <div className="question-show-page">
-              <div className="qshow-top">
-                <p>{this.props.question.title}</p>
-                <div className="ask-question">
-                    <Link to="questions/new">Ask Question</Link>
-                </div>   
-              </div>
-              
-              <ReactQuill
-                  className="qshow-form"
-                  value={this.props.question.body}
-                  readOnly={true}
-                  modules={{toolbar: false}}
-                />
-
-              {this.props.question.author_id === this.props.currentUserId ? 
-                (<div className="qshow-buttons">
-                  <Link to={`/questions/${this.props.questionId}/edit`}>
-                    edit
-                  </Link>
-                  <a onClick={this.deleteQuestion}>
-                    delete
-                  </a>
-                </div>) : null}
-
-                  {this.props.question.answers ? 
-                    (<ul>
-                        <p>{this.props.question.answers.length} Answers</p>
-                        {this.props.question.answers.map((answer, idx) => (
-                        <li key={`answer${idx}`}>
-                          <AnswerContainer questionId={this.props.questionId} answer={answer}/>
-                        </li>
-                      ))}
-                      </ul>): null}
-
-              <AnswerFormContainer questionId={this.props.questionId}/>
-              <Footer/>
+          <Sidebar />
+          <div className="question-show-page">
+            <div className="qshow-top">
+              <p>{this.props.question.title}</p>
+              <div className="ask-question">
+                  <Link to="questions/new">Ask Question</Link>
+              </div>   
             </div>
+            
+            <ReactQuill
+                className="qshow-form"
+                value={this.props.question.body}
+                readOnly={true}
+                modules={{toolbar: false}}
+            />
+
+            {this.props.question.author_id === this.props.currentUserId ? 
+              (<div className="qshow-buttons">
+                <Link to={`/questions/${this.props.questionId}/edit`}>
+                  edit
+                </Link>
+                <a onClick={this.deleteQuestion}>
+                  delete
+                </a>
+              </div>) : null}
+
+                {this.props.question.answers ? 
+                  (<ul>
+                      <p>{this.props.question.answers.length} Answers</p>
+                      {this.props.question.answers.map((answer, idx) => (
+                      <li key={`answer${idx}`}>
+                        <AnswerContainer questionId={this.props.questionId} answer={answer}/>
+                      </li>
+                    ))}
+                    </ul>): null}
+
+            <AnswerFormContainer questionId={this.props.questionId}/>
+            <Footer/>
+          </div>
           </>
         );
       }
