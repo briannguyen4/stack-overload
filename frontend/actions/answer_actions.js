b1c3fcc52af60f31a00b25c659937f002f11aed0
briannguyen4 <brian4nguyen@gmail.com>
2020-09-23 03:04:00 -0400

    add answer index actions

--- frontend/actions/answer_actions.js
+++ frontend/actions/answer_actions.js
@@ -2,10 +2,16 @@ import * as APIUtil from './../util/answer_api_util';
 import * as VoteAPIUtil from '../util/vote_api_util';
 
 export const RECEIVE_ANSWER = 'RECEIVE_ANSWER';
+export const RECEIVE_ANSWERS = 'RECEIVE_ANSWERS';
 
 const receiveAnswer = (answer) => ({
     type: RECEIVE_ANSWER,
     answer,
+});
+
+export const receiveAnswers = answers => ({
+    type: RECEIVE_ANSWERS,
+    answers
 });
 
 export const createAnswer = (answer, questionId) => dispatch => {
@@ -19,6 +25,13 @@ export const getAnswer = (answerId, questionId) => dispatch => {
         .then(answer => dispatch(receiveAnswer(answer)))
     );
 }
+
+export const getAnswers = () => dispatch => {
+    return (
+      APIUtil.getAnswers()
+        .then(answers => dispatch(receiveAnswers(answers)))
+    );
+};
 
 export const upvoteAnswer = (questionId, answerId) => {
     return dispatch => {
