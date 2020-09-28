d7e1ac30a70f01fc68c35f18ca2675dc7f4b6c12
briannguyen4 <brian4nguyen@gmail.com>
2020-09-28 01:05:00 -0400

    create search controller action

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,15 @@
 class MainController < ActionController::Base  
-    def search()
-      
+    before_action :force_json, only: :search
+
+    def search
+        @questions = Question.includes(:tags).ransack(title_cont: params[:q]).result(distinct:true)
+    end  
+
+  private
+    
+    def force_json
+        request.format = :json
     end
-  
-  
-  end
+end
+
   
