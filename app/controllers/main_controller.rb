2085ffbea1a7d68886b42ee8e2497cae11481f46
briannguyen4 <brian4nguyen@gmail.com>
2020-09-29 08:37:16 -0400

    add search jbuilder

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,7 @@ class MainController < ActionController::Base
     before_action :force_json, only: :search
 
     def search
-        @questions = Question.includes(:tags).ransack(title_cont: params[:q]).result(distinct:true)
+        @questions = Question.ransack(title_cont: params[:q]).result(distinct:true)
     end  
 
   private
--- frontend/components/questions/question_search.jsx
+++ frontend/components/questions/question_search.jsx
@@ -14,9 +14,9 @@ class QuestionSearch extends React.Component {
     }
 
     render() {
-        return (
+        // return (
               
-                )
+        //         );
     }
 }
 
