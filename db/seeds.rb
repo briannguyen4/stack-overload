a59a7471d2f9af384a6cf693ea02e649189ba1a9
briannguyen4 <brian4nguyen@gmail.com>
2020-11-01 02:04:00 -0500

    add seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,15 +2,15 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+# Character.create(name: 'Luke', movie: movies.first)
+
 Vote.destroy_all
 Answer.destroy_all
 Question.destroy_all
 User.destroy_all
 
-demo_user = User.create({email: "demouser@email.com", password: "password"})
+demo = User.create({email: "demouser@email.com", password: "password"})
 user1 = User.create({email: "email1@email.com", password: "password"})
 user2 = User.create({email: "email2@email.com", password: "password"})
 user3 = User.create({email: "email3@email.com", password: "password"})
@@ -56,9 +56,27 @@ user42 = User.create({email: "email42@email.com", password: "password"})
 user43 = User.create({email: "email43@email.com", password: "password"})
 user44 = User.create({email: "email44@email.com", password: "password"})
 
-q1 = Question.create({title: "Whats the difference between == and === operators?", body: "Hello, whats the difference between == and === in JavaScript? I've seen != and !== operators as well. Are there more operators?", author_id: user1.id})
+q1 = Question.create({title: "Whats different between == and === ?", body: "Hello, whats the difference between == and === in JavaScript? I've seen != and !== operators as well. Are there more operators?", author_id: user1.id})
 q2 = Question.create({title: "What are ownProps in react-redux?", body: "This is a test to see if I can properly create a question", author_id: user1.id})
 q3 = Question.create({title: "Why is this.props.match.params empty when I pass in params", body: "I tried to pass params to this.props.match in my React component but the params still return as an empty object. Can anyone help me with figure out why?", author_id: user1.id})
 q4 = Question.create({title: "How do you use CSS to get a nav bar to stay at the top of the page? ", body: "I have a navbar but for some reason it won't stay on the top of my page, how do I solve this with CSS?", author_id: user2.id})
 q5 = Question.create({title: "Cannot read property x of undefined error, why?", body: "Hey, for some reason I keep getting this error when I try to key into my props, why is that?", author_id: user2.id})
-q6 = Question.create({title: "How does the react and redux cycle work?", body: "I've recently started to learn react and redux but I still can't completely wrap my head around how it works, does anyone have a good high overview?", author_id: user2.id})
+q6 = Question.create({title: "How does the react and redux cycle work?", body: "I've recently started to learn react and redux but I still can't completely wrap my head around how it works, does anyone have a good high overview?", author_id: demo_user2.id})
+q7 = Question.create({title: "", body: "", author_id: .id})
+q8 = Question.create({title: "", body: "", author_id: .id})
+q9 = Question.create({title: "", body: "", author_id: .id})
+q10 = Question.create({title: "", body: "", author_id: .id})
+q11 = Question.create({title: "", body: "", author_id: .id})
+q12 = Question.create({title: "", body: "", author_id: .id})
+q13 = Question.create({title: "", body: "", author_id: .id})
+q14 = Question.create({title: "", body: "", author_id: .id})
+q15 = Question.create({title: "", body: "", author_id: .id})
+q16 = Question.create({title: "", body: "", author_id: .id})
+q17 = Question.create({title: "", body: "", author_id: .id})
+q18 = Question.create({title: "", body: "", author_id: .id})
+q19 = Question.create({title: "", body: "", author_id: .id})
+q20 = Question.create({title: "", body: "", author_id: .id})
+q21 = Question.create({title: "", body: "", author_id: .id})
+q22 = Question.create({title: "", body: "", author_id: .id})
+q23 = Question.create({title: "", body: "", author_id: .id})
+q24 = Question.create({title: "", body: "", author_id: .id})
