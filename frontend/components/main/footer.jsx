0821cc8348a525ff92cb979f241b5a5c1b2f37ca
briannguyen4 <brian4nguyen@gmail.com>
2020-11-17 02:43:23 -0500

    style footer

--- frontend/components/main/footer.jsx
+++ frontend/components/main/footer.jsx
@@ -10,11 +10,15 @@ const Footer = () => (
             <img src={window.iconURL}/>
         </Link>
         <Link to="/">
-            <p>STACK OVERLOAD</p>
+            <p className="footer-title">STACK OVERLOAD</p>
         </Link>
         <div className="disclaimer">
             <a href="https://github.com/briannguyen4/stack-overload/">
                 <p id="github">Github</p>
+                
+            </a>
+            <a href="https://www.linkedin.com/in/brian-nguyen-1293bb148/">
+                <p id="linkedin">LinkedIn</p>
             </a>
             <p>site design / logo © 2020 Stack Overload</p>
         </div>
