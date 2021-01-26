626ad9eb2f35f9d4c990ef7c3c51a4b85efd42b8
briannguyen4 <brian4nguyen@gmail.com>
2021-01-26 05:03:36 -0500

    convert sidebar css to scss

--- frontend/components/main/sidebar.jsx
+++ frontend/components/main/sidebar.jsx
@@ -6,18 +6,20 @@ import { faGlobeAmericas } from '@fortawesome/free-solid-svg-icons';
 const Sidebar = () => (
   <>
   <div className="sidebar">
-    <div className="sidebar-content">
-      <div className="home">
+    <div className="sidebar__content">
+      <div className="sidebar__content__home">
         <Link to="/questions">
           <li>Home</li>
         </Link>
       </div>
-      <p>PUBLIC</p>
-      <div className="public">
-        <FontAwesomeIcon icon={faGlobeAmericas} />
-        <Link to="/questions">
-          <li>Stack Overload</li>
-        </Link>
+      <div className="sidebar__content__public">
+        <p className="sidebar__content__public__text">PUBLIC</p>
+        <div className="sidebar__content__public__container">
+          <FontAwesomeIcon icon={faGlobeAmericas} />
+          <Link to="/questions">
+            <li>Stack Overload</li>
+          </Link>
+        </div>
       </div>
     </div>
   </div>
