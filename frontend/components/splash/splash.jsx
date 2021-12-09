1d874d33ce120f9d41621ad1c3cf57568dd3daaf
briannguyen4 <brian4nguyen@gmail.com>
2021-12-09 18:52:10 -0500

    update splash page

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -10,18 +10,18 @@ const Splash = () => (
             <img src={window.splashURL}/>
             <div className="splash-img__text">
                 <h1 className="splash-img__text__header">We &#60;3 people who code</h1>
-                <p className="splash-img__text__sentence">We build products that empower developers
-                and connect them to solutions that enable productivity, growth, and discovery.</p>
+                <span className="splash-img__text__sentence">We build products that empower developers
+                and connect them to solutions that enable productivity, growth, and discovery.</span>
             </div>
         </div>
         <div className="splash-message">
             <h1 className="splash-message__header">For developers, by developers</h1>
             <div className="splash-message__bar"></div>
-            <div className="splash-message__text">
-                <p>Welcome to Stack Overload, an open community for all coders. Get answers immediately
-                 from a community of knowledgeable programmers. Search through a collection of questions
-                 or create an account to ask your own!</p>
-            </div>    
+            <p className="splash-message__text">
+                Welcome to Stack Overload, an open community for all coders. Get answers immediately
+                from a community of knowledgeable programmers. Search through a collection of questions
+                or create an account to ask your own!
+            </p>    
         </div>
     </div>
     <div className="splash-footer"><Footer/></div>
